6323366d1ab08bb9b6073e1501baec521c5affb8
Chris Forbes <djforeskin@ucc.gu.uwa.edu.au>
2017-03-24 17:28:45 +0800

    Worked on Hello message and made a name input line

--- src/HellowWorld.cpp
+++ src/HellowWorld.cpp
@@ -1,8 +1,15 @@
-#include <iostream> 
+#include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
-	cout << "Hellow UCC!" << endl;
+	string name;
+	cout << "Input Name:" << name << endl;
+	cin >> name;
+	cout << "Hello, " << name << endl;
+
 	return 0;
-}
+} 
+
