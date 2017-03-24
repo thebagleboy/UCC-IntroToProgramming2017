6449e3159d9bf19ef2391aeac18ceddc378a42ac
thebagleboy <alfred.burgess95@gmail.com>
2017-03-24 09:54:40 +0800

    Added the files from last week, inclusing comments

--- src/fib.cpp
+++ src/fib.cpp
@@ -1,10 +1,20 @@
+// Include the Input/Output library from the _Standard Library_
 #include <iostream>
+
+// Use the _namespace_ "std", also known as standard
 using namespace std;
 
-int fib(int a, int b, int limit)
+/* Create a function called "fib" which takes in one arguement;
+ *   the depth of the search in the sequence
+ */
+int fib(int limit)
 {
+// Initialize our variables
+  int a = 0; int b = 1;
   int i = 0;
   int c = 0;
+
+  // Create a loop
   while(i < limit)
   {
     // This is where we do our work
@@ -12,13 +22,17 @@ int fib(int a, int b, int limit)
     a = b;
     b = c;
     i = i + 1;
-//    cout << c << endl;
   }
   return c;
 }
 
+/* Our main function
+ * This is where our program will start
+ */
 int main()
 {
-	cout << fib(2, 1, 10) << endl;
+	// Call our function and output the results
+	cout << fib(10) << endl;
+	// Everything worked so return 0
 	return 0;
 }
