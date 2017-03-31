b1d0e17e17833e1bec5215e0e636f571795a4d33
Chris Forbes <djforeskin@ucc.gu.uwa.edu.au>
2017-03-31 17:18:55 +0800

    I did stuff with characters today

---
+++ char.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+//exploring characters and numbers
+int main()
+{
+	/*
+	char glyph;
+	cin >> glyph;
+	*/
+	
+	int glyph;
+	cin >> glyph;
+	printf("%c\n", (char)glyph);
+	return 0;
+}
