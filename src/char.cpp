b2b69a697644f0a6c1a22eac899f8e8b11c56749
Gavin Tay Fernandez <tayfga15@ucc.gu.uwa.edu.au>
2017-03-31 17:17:37 +0800

    Week 2

---
+++ src/char.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	/*
+	char glyph;
+	cin >> glyph;
+	*/
+	int glyph;
+	cin >> glyph;
+	printf("%c\n", (char)glyph);
+	return 0;
+}
